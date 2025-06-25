8aa0ed001a3d2a1c2d8f5607473bf12d2e0d7f73
Gideon Ofori Addo <gideonaddoofori@gmail.com>
2025-06-25 14:03:44 +0000

    Refactor sendPasswordResetEmail function: update to use object destructuring for parameters and improve code formatting for better readability

--- src/lib/email-templates/send-password-reset-email.ts
+++ src/lib/email-templates/send-password-reset-email.ts
@@ -1,29 +1,36 @@
-import { render } from "@react-email/render";
-import { PasswordResetEmail } from "./password-reset-email";
-import { Resend } from "resend";
+import { render } from "@react-email/render"
+import { Resend } from "resend"
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-const from = process.env.SENDER_EMAIL_ADDRESS!;
-const baseUrl = process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000";
+import { PasswordResetEmail } from "./password-reset-email"
 
-export const sendPasswordResetEmail = async (email: string, token: string) => {
-    const resetLink = `${baseUrl}/auth/reset-password?token=${token}`;
+const resend = new Resend(process.env.RESEND_API_KEY)
+const from = process.env.SENDER_EMAIL_ADDRESS!
+const baseUrl = process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000"
 
-    const emailHtml = await render(
-        PasswordResetEmail({
-            resetLink,
-        }),
-    );
+export async function sendPasswordResetEmail({
+  email,
+  token,
+}: {
+  email: string
+  token: string
+}) {
+  const resetLink = `${baseUrl}/auth/reset-password?token=${token}`
 
-    try {
-        await resend.emails.send({
-            from,
-            to: email,
-            subject: "Reset your password",
-            html: emailHtml,
-        });
-    } catch (error) {
-        console.error(error);
-        throw new Error("Failed to send password reset email.");
-    }
-}; 
+  const emailHtml = await render(
+    PasswordResetEmail({
+      resetLink,
+    })
+  )
+
+  try {
+    await resend.emails.send({
+      from,
+      to: email,
+      subject: "Reset your password",
+      html: emailHtml,
+    })
+  } catch (error) {
+    console.error(error)
+    throw new Error("Failed to send password reset email.")
+  }
+}
