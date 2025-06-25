4addbe84a36a002fc7d33df341b579506f114278
Gideon Ofori Addo <gideonaddoofori@gmail.com>
2025-06-25 17:35:54 +0000

    Implement middleware for authentication: add public and auth route handling, redirect users based on authentication status, and configure request matching for API routes.

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,59 @@
-export { auth as middleware } from "@/lib/auth/auth"
+import { NextResponse } from "next/server"
+
+import { auth } from "@/lib/auth/auth"
+
+// TODO: Add more public routes or update as needed
+const PUBLIC_ROUTES = ["/auth/signin", "/auth/signup", "/auth/reset-password"]
+const AUTH_ROUTES = ["/auth/signin", "/auth/signup", "/auth/reset-password"]
+const API_AUTH_PREFIX = ["/api/auth"]
+
+export default auth((req) => {
+  const { nextUrl } = req
+  const isLoggedIn = !!req.auth
+  const isAuthRoute = AUTH_ROUTES.some((route) =>
+    nextUrl.pathname.startsWith(route)
+  )
+  const isPublicRoute = PUBLIC_ROUTES.some((route) =>
+    nextUrl.pathname.startsWith(route)
+  )
+  const isApiAuthRoute = API_AUTH_PREFIX.some((prefix) =>
+    nextUrl.pathname.startsWith(prefix)
+  )
+
+  if (isApiAuthRoute) {
+    return NextResponse.next()
+  }
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return NextResponse.redirect(new URL("/portal", nextUrl))
+    }
+    return NextResponse.next()
+  }
+
+  if (!isPublicRoute && !isLoggedIn) {
+    let callbackUrl = nextUrl.pathname
+    if (nextUrl.search) {
+      callbackUrl += nextUrl.search
+    }
+
+    return NextResponse.redirect(
+      new URL(
+        `/auth/signin?callbackUrl=${encodeURIComponent(callbackUrl)}`,
+        nextUrl
+      )
+    )
+  }
+
+  return NextResponse.next()
+})
+
+export const config = {
+  matcher: [
+    // "/api/:path*", "/((?!$).*)"
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
+}
