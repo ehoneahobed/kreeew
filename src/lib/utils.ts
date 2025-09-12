514b3aadbba2f8b108937fd1faf1cf17a0c03cd9
Gideon Ofori Addo <gideonaddoofori@gmail.com>
2025-09-12 21:24:42 +0000

    Add JWT token generation and verification utilities: implement `generateToken` and `verifyToken` functions for secure token handling, enhancing authentication capabilities.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,6 @@
 import bcrypt from "bcryptjs"
 import { clsx, type ClassValue } from "clsx"
+import jwt, { JsonWebTokenError, JwtPayload, SignOptions } from "jsonwebtoken"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
@@ -48,5 +49,63 @@ export async function verifyPassword({
     return result
   } catch (error) {
     throw new Error("Failed to verify password", { cause: error })
+  }
+}
+
+type JWTTokenProps = {
+  email?: string
+  userId?: string
+}
+
+/**
+ * The function generates a JWT token with a specified role and email, using a secret key and setting
+ * an expiration time of 1 hour.
+ * @param {JWTTokenProps}  - The `generateToken` function takes in an object with two properties:
+ * `email`. It then generates a JSON Web Token (JWT) using the `jwt.sign` method with the
+ * provided `email` values, along with a secret key (`JWT_SECRET`)
+ * @returns The function `generateToken` is returning a JSON Web Token (JWT) that is generated using
+ * the `jwt.sign` method with the provided `email` parameters, along with a `JWT_SECRET` and
+ * an expiration time of 1 hour.
+ */
+export function generateToken(
+  options: JWTTokenProps,
+  expiresIn: SignOptions["expiresIn"] = "24h"
+): string {
+  const token = jwt.sign(options, process.env.JWT_SECRET!, {
+    expiresIn,
+  })
+  return token
+}
+
+interface TokenVerificationResult {
+  valid: boolean
+  expired: boolean
+  decoded?: string | JwtPayload
+}
+
+/**
+ * The function `verifyToken` verifies a token using a secret key and returns a result indicating if
+ * the token is valid, expired, or invalid.
+ * @param {string} token - The `token` parameter is a string that represents a JSON Web Token (JWT)
+ * that needs to be verified for its validity and expiration status. The `verifyToken` function takes
+ * this token as input and uses the `jwt.verify` method to decode and verify the token using a secret
+ * key (`JWT
+ * @returns The `verifyToken` function returns a `TokenVerificationResult` object. This object contains
+ * information about the verification result, including whether the token is valid, expired, and the
+ * decoded token data if it is valid.
+ */
+export function verifyToken(token: string): TokenVerificationResult {
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!)
+    return { valid: true, expired: false, decoded }
+  } catch (error) {
+    if (
+      error instanceof JsonWebTokenError &&
+      error.name === "TokenExpiredError"
+    ) {
+      return { valid: false, expired: true }
+    } else {
+      return { valid: false, expired: false }
+    }
   }
 }
